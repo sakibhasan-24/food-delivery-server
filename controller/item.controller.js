a617ef6dbc25e5a5b125115bb35c58e5318c0f86
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-03-28 01:34:41 +0600

    delete items api

--- controller/item.controller.js
+++ controller/item.controller.js
@@ -22,3 +22,82 @@ export const createItems = async (req, res) => {
       .json({ message: "Internal server error", success: false });
   }
 };
+
+export const getItems = async (req, res) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const pageSize = parseInt(req.query.pageSize) || 6;
+    //  const items = await Item.find().skip(startIndex).limit(pageSize);
+    const sort = req.query.order === "asc" ? 1 : -1;
+    const priceHighToLow = req.query.price === "desc" ? -1 : 1;
+    const items = await Item.find({
+      ...(req.query.category && {
+        category: req.query.category,
+      }),
+      ...(req.query.name && {
+        name: { $regex: req.query.name, $options: "i" },
+      }),
+      ...(req.query.price && {
+        price: { $gte: parseInt(req.query.price) },
+      }),
+      ...(req.query.itemId && {
+        _id: req.query.itemId,
+      }),
+      ...(req.query.userId && {
+        userId: req.query.userId,
+      }),
+      ...(req.query.searchTerm && {
+        name: { $regex: req.query.searchTerm, $options: "i" },
+        description: { $regex: req.query.searchTerm, $options: "i" },
+        category: { $regex: req.query.searchTerm, $options: "i" },
+        ingredients: { $regex: req.query.searchTerm, $options: "i" },
+      }),
+    })
+      .sort({ price: priceHighToLow })
+      .skip(startIndex * pageSize)
+      .limit(pageSize);
+
+    const totalItems = await Item.countDocuments();
+    return res.status(200).json({
+      items,
+      totalItems,
+      message: "Items fetched successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Internal server error", success: false });
+  }
+};
+
+export const deleteItem = async (req, res) => {
+  // console.log(req.user);
+  if (!req.user.isAdmin) {
+    return res.status(401).json({
+      message: "You are not authorized to perform this action",
+      success: false,
+    });
+  }
+  try {
+    const item = await Item.findById(req.params.id);
+    // console.log(item);
+    if (!item) {
+      return res.status(404).json({
+        message: "Item not found",
+        success: false,
+      });
+    }
+    const result = await Item.deleteOne({ _id: req.params.id });
+    return res.status(200).json({
+      success: true,
+      result,
+      message: "successfully deleted",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", success: false });
+  }
+};
--- routes/item.routes.js
+++ routes/item.routes.js
@@ -1,8 +1,14 @@
 import express from "express";
 import { verifyToken } from "../helper/verifyToken.js";
-import { createItems } from "../controller/item.controller.js";
+import {
+  createItems,
+  deleteItem,
+  getItems,
+} from "../controller/item.controller.js";
 
 const router = express.Router();
 
 router.post("/add-items", verifyToken, createItems);
+router.get("/get-items", getItems);
+router.delete("/delete-item/:id", verifyToken, deleteItem);
 export default router;
